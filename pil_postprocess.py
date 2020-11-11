227f495f68a06046c5b0e1509ae1d5810332da1d
Marco Gulino <marco.gulino@gmail.com>
2020-11-11 15:59:45 +0000

    More logging

--- pil_postprocess.py
+++ pil_postprocess.py
@@ -6,12 +6,14 @@ from multiprocessing import Process
 
 
 def pil_annotate(filename):
+    started = time.time()
     img = Image.open(filename)
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(settings.pil_annotate_text_font, settings.pil_annotate_text_size)
     text = time.strftime(settings.pil_annotate_time_format) if settings.pil_annotate_time_format else settings.pil_annotate_text
     draw.text(settings.pil_annotate_text_position, text, settings.pil_annotate_foreground,font=font)
     img.save(filename)
+    logging.debug('pil_annotate: file={}, text="{}", elapsed: {}'.format(filename, text, time.time() - started))
 
 
 
